4d7f6025b082036decf65fd067808f193752dde6
Arya <aryailham05@users.github.com>
2020-08-20 18:52:36 +0700

    add bxslider

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.slider').bxSlider({
+        mode: 'horizontal',
+        pager: false,
+        responsive: true,
+        auto: false,
+        infiniteLoop: false,
+        hideControlOnEnd: true
+    });
+});
